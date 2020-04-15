5e3ccdc689c0871292f4dddebfc72872c1cc868b
jae57 <ggapsong@naver.com>
2020-04-15 10:02:03 +0900

    map to array return in bookmark api

--- src/main/java/com/na/backend/mapper/UserMapper.java
+++ src/main/java/com/na/backend/mapper/UserMapper.java
@@ -1,12 +1,15 @@
 package com.na.backend.mapper;
 
+import com.na.backend.dto.BookmarkQuestionDto;
 import com.na.backend.dto.UserAuthDto;
 import com.na.backend.dto.UserInfoDto;
+import com.na.backend.entity.Question;
 import com.na.backend.entity.User;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class UserMapper {
@@ -32,6 +35,22 @@ public class UserMapper {
                 .token(user.getToken())
                 .name(user.getName())
                 .build();
+    }
+
+    public List<Question> toQuestions(Map<String, BookmarkQuestionDto> bookmarkQuestionDtos) {
+        List<Question> bookmarkQuestions = new ArrayList<>();
+        for(String key: bookmarkQuestionDtos.keySet()) {
+            BookmarkQuestionDto bookmarkQuestionDto = bookmarkQuestionDtos.get(key);
+            bookmarkQuestions.add(Question.builder()
+                    .id(key)
+                    .content(bookmarkQuestionDto.getContent())
+                    .category(bookmarkQuestionDto.getCategory())
+                    .type(bookmarkQuestionDto.getType())
+                    .choices(bookmarkQuestionDto.getChoices())
+                    .build());
+        }
+
+        return bookmarkQuestions;
     }
 
 }
