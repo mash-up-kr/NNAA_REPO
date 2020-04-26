0f807a5f161f7bf3fdb24509817bef750a78ab3c
jae57 <ggapsong@naver.com>
2020-04-26 11:26:00 +0900

    fix link

--- src/main/java/com/na/backend/util/MailManager.java
+++ src/main/java/com/na/backend/util/MailManager.java
@@ -27,7 +27,7 @@ public class MailManager {
         String resetMessage = new StringBuffer().append("<p>" + user.getName() + "님, 안녕하세요!</p>")
                 .append("<p>비밀번호를 다시 설정하고 싶으신가요?</p>")
                 .append("<p><b>아래 링크</b>를 눌러주세요!</p>")
-                .append("<a href='nnaa://resetPassword?id=")
+                .append("<a href='nnaa://reset_password?id=")
                 .append(user.getId())
                 .append("&token=")
                 .append(user.getToken())
